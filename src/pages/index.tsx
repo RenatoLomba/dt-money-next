b1abc1b8084ea5a19600d804d614e5c3a69f7437
Renato Lomba <rntlomba@gmail.com>
2022-08-11 20:14:24 -0300

    Show transactions only for signed users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from 'next'
+import { useSession } from 'next-auth/react'
 
 import { Header } from '../components/header'
 import { Summary } from '../components/summary'
 import { Transactions } from '../components/transactions'
 
 const Home: NextPage = () => {
+  const { data: session } = useSession()
+
   return (
     <div>
       <Header />
 
-      <Summary />
+      {session && (
+        <>
+          <Summary />
 
-      <Transactions />
+          <Transactions />
+        </>
+      )}
     </div>
   )
 }
